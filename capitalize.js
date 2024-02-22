d420df084c10431305ca84e4107c3b1671074a81
Vitali Hoila <vitali.hoila@coverwallet.com>
2024-02-22 14:39:26 +0300

    Create function capitalize on String.protirype

---
+++ capitalize.js
@@ -0,0 +1,19 @@
+ /**
+ * Task 6
+ * Add method "capitalize" to strings which should work as described below
+ */
+
+ String.prototype.capitalize = function () {
+  console.log(this)
+  let str = this.toLocaleLowerCase().split(' ')
+  let result = [];
+  for (let row of str) {
+      // result.push(row.at(0).toUpperCase() + row.slice(1))
+      result.push(row.replace(/^./, row.at(0).toUpperCase()))
+  }
+
+  return console.log(result.join(' '))
+}
+
+console.log("hello world".capitalize() === "Hello World");
+console.log("OH MY".capitalize() === "Oh My")
