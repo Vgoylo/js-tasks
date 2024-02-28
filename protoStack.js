34dbab0c357e4075bdde2e60b5c1505fbefc44ef
Vitali Hoila <vgoylo91mav@gmail.com>
2024-02-28 22:24:57 +0300

    Create function with hew operator and add funtions into prototype

---
+++ protoStack.js
@@ -0,0 +1,37 @@
+/**
+ * Task 1
+ * Please make you own "Stack" data structure (check https://www.youtube.com/watch?v=KcT3aVgrrpU)
+ * Please use functions-constructors to do that
+ * See example usage below
+ */
+
+function Stack() {
+  this.obj = [];
+  
+}
+
+Stack.prototype = {
+  push() {
+    return this.obj.pop();
+  },
+  pop() {
+    return this.obj.pop();
+  },
+  log() {
+    return this.obj.length;
+  },
+};
+
+let numbersStack = new Stack();
+
+console.log("numbersStack.push(1), returns nothing => ", numbersStack.push(1)); // adds 1, returns nothing
+console.log("numbersStack.push(2), returns nothing => ", numbersStack.push(2)); // adds 2, returns nothing
+console.log("numbersStack.push(3), returns nothing => ", numbersStack.push(3)); // adds 3, returns nothing
+console.log("numbersStack.size returns 3 => ", numbersStack.size); // returns 3
+console.log("return logs 1, 2, 3 to console => ", numbersStack.log()); // logs 1, 2, 3 to console
+console.log("removes 3 from stack and returns it => ", numbersStack.pop()); // removes 3 from stack and returns it
+console.log("numbersStack.size returns 2 => ", numbersStack.size); // returns 2
+console.log("removes 2 from stack and returns it => ", numbersStack.pop()); // removes 2 from stack and returns it
+console.log("numbersStack.size returns 1 => ", numbersStack.size); // returns 1
+console.log("numbersStack.push(4), returns nothing => ", numbersStack.push(4)); // adds 4, returns nothing
+console.log("return logs 1, 4 to console", numbersStack.log()); // logs 1, 4 to console
