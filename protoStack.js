d88f4441ebf789c1462b768ed41475b580a11b50
Vitali Hoila <vgoylo91mav@gmail.com>
2024-02-28 22:37:53 +0300

    Anothe decide

--- protoStack.js
+++ protoStack.js
@@ -7,27 +7,33 @@
 
 function Stack() {
   this.obj = [];
-  
-}
+  // this.size = size;
 
-Stack.prototype = {
-  push() {
-    return this.obj.pop();
-  },
-  pop() {
-    return this.obj.pop();
-  },
-  log() {
+  Stack.prototype.push = function (num) {
+    this.obj.push(num);
+    // console.log(this.obj);
+    return "";
+  };
+
+  Stack.prototype.pop = function (num) {
+    return this.obj.pop(num);
+  };
+
+  Stack.prototype.log = function () {
+    return this.obj.toString();
+  };
+
+  Stack.prototype.size = function () {
     return this.obj.length;
-  },
-};
+  };
+}
 
 let numbersStack = new Stack();
 
 console.log("numbersStack.push(1), returns nothing => ", numbersStack.push(1)); // adds 1, returns nothing
 console.log("numbersStack.push(2), returns nothing => ", numbersStack.push(2)); // adds 2, returns nothing
 console.log("numbersStack.push(3), returns nothing => ", numbersStack.push(3)); // adds 3, returns nothing
-console.log("numbersStack.size returns 3 => ", numbersStack.size); // returns 3
+console.log("numbersStack.size returns 3 => ", numbersStack.size()); // returns 3
 console.log("return logs 1, 2, 3 to console => ", numbersStack.log()); // logs 1, 2, 3 to console
 console.log("removes 3 from stack and returns it => ", numbersStack.pop()); // removes 3 from stack and returns it
 console.log("numbersStack.size returns 2 => ", numbersStack.size); // returns 2
