396cd3d79a69c7e309c49d9a5027eb1aa0aaf468
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-12 14:58:31 +0300

    Midifened promise.js added addres in object

--- promise.js
+++ promise.js
@@ -5,14 +5,32 @@ let Users = [
   {
     name: "vitali",
     age: 32,
+    address: {
+      street: "semashko",
+      numberBuild: 15,
+      floo: 23,
+    },
+    email: "vg@mail.com",
   },
   {
     name: "alesy",
     age: 32,
+    address: {
+      street: "Nemiga",
+      numberBuild: 156,
+      floo: 13,
+    },
+    email: "ag@mail.com",
   },
   {
     name: "matvey",
     age: 5,
+    address: {
+      street: "Derginckogo",
+      numberBuild: 115,
+      floo: 12,
+    },
+    email: "mg@mail.com",
   },
 ];
 
@@ -21,5 +39,8 @@ function getUserName(num) {
 }
 
 getUserName(1)
-  .then((user) => user.name)
-  .then((name) => console.log(name));
+  .then((user) => user.address)
+  .then((address) => address.street)
+  .then((street) => console.log(street));
+
+console.log("dddd");
